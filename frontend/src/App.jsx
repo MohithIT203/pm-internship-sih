6d07f97033fcb461fdf1703fa845b03f2cbc00fc
HARJIT M <harjitrm@gmail.com>
2025-09-12 15:08:37 +0530

    login and signin page added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Login from "./components/login";import "./App.css";
+import Signin from "./components/signin";
+function App() {
+  return (
+    <Login/>
+    // <Signin />
+  );
+}
+
+export default App;
