d989b6e2417d044141f50ebb50fbfeb2fd0524d1
harrish159 <jkhr2005@gmail.com>
2025-09-12 15:45:57 +0530

    Login And SignUp Integrated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,16 @@ import React from "react";
 import Landing from "./pages/landing";
 import Dashboard from "./pages/dashboard";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./components/login";
+import SigninPage from "./components/signin";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/signup" element={<SigninPage/>} />
       </Routes>
   );
 }
